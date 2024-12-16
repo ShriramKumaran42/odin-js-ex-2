ed9049e6b53c9b4bb15f830161a5ceb902a6af6e
ShriramKumaran42 <shriramkumaran142000@gmail.com>
2024-12-17 01:38:13 +0530

    finished the whole proj
    added different hover colors using math func,
    added reset and clear buttons.

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
+
+
 const container = document.querySelector("#container");
 
-//let sq = prompt("enter the number of n x n square grids to be created (max  - 100) ");
+let sq = prompt("enter the number of n x n square grids to be created (max: 100) ");
+if(sq==NaN || sq < 1 || sq > 100){
+    alert("Invalid input, Default value 16 is set. If you wish to Change click the Reset Button");
+    sq = 16;
+}
+else{
+    sq = Number(sq);
+}
+
+makeDivs(16);
+
+function randomColors() {
+    const colorsArray = ["#ff3300", "#009900", "#ffff00", "#00cc99", "#0033cc", "#66ffff",
+        "#ff00ff", "#ff0066", "#ff6600", "#996600"
+    ];
+    
+    const randomArray = Math.floor(Math.random()*colorsArray.length);
+    return colorsArray[randomArray];
+}
 
 function makeDivs(nDivs){
+    container.innerHTML = "";
+
     for(let i = 0; i<nDivs; i++){
         let rows = document.createElement("div");
         rows.classList.add('rows');
@@ -11,7 +33,7 @@ function makeDivs(nDivs){
             cols.classList.add("cols");
 
             cols.addEventListener("mouseover", () => {
-                cols.style.backgroundColor="red";
+                cols.style.backgroundColor= randomColors();
             })
             rows.appendChild(cols);
         }
@@ -19,15 +41,31 @@ function makeDivs(nDivs){
     }
 }
 
-const rBtn = document.querySelector("#reset");
 const cBtn = document.querySelector("#clear");
+cBtn.addEventListener("click", () => {
+    const cols = document.querySelectorAll(".cols");
+    cols.forEach(cols => {
+        cols.style.backgroundColor = "";
+    });
+});
 
-cBtn.addEventListener("click", (event) => {
-    event.target.style.backgroundColor = "";
-})
+const rBtn = document.querySelector("#gridReset");
+rBtn.addEventListener("click", () => {
+    let sqNew = prompt("enter the num of squares the grid has to reset. (max : 100)");
+    if(sqNew == NaN || sqNew < 1 || sqNew > 100){
+        alert("Invalid Number.");
+        return;
+    }
+    makeDivs(Number(sqNew));
+});
 
 
-//makeDivs(sq);
-makeDivs(16);
+
+
+
+
+
+
+
 
 
